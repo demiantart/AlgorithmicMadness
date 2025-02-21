7091e276a04801dc9087048528c497d0baf2c68f
demiantart <t.m.xavier1-24@student.lboro.ac.uk>
2025-02-21 00:54:33 +0000

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -1,31 +1,9 @@
 let notifications = [];
-let tiltX = 0;
-let tiltY = 0;
-const TILT_INFLUENCE = 15; // Adjust this to control tilt sensitivity
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   
-  // Request permission for device orientation if needed
-  if (typeof DeviceOrientationEvent !== 'undefined' && typeof DeviceOrientationEvent.requestPermission === 'function') {
-    document.addEventListener('click', requestAccess);
-  }
-}
-
-function requestAccess() {
-  DeviceOrientationEvent.requestPermission()
-    .then(response => {
-      if (response == 'granted') {
-        window.addEventListener('deviceorientation', handleOrientation);
-      }
-    })
-    .catch(console.error);
-}
-
-function handleOrientation(event) {
-  // Convert orientation data to usable values
-  tiltX = map(event.gamma, -90, 90, -1, 1);  // Left/Right tilt
-  tiltY = map(event.beta, -90, 90, -1, 1);   // Front/Back tilt
 }
 
 function draw() {
@@ -36,15 +14,10 @@ function draw() {
     notifications.push(new Notification());
   }
   
-  // Update and display notifications
+  // Update and display notifications from bottom to top
   for (let i = notifications.length - 1; i >= 0; i--) {
     notifications[i].update();
     notifications[i].show();
-    
-    // Remove notifications that are off screen
-    if (notifications[i].offScreen()) {
-      notifications.splice(i, 1);
-    }
   }
 }
 
@@ -52,36 +25,22 @@ class Notification {
   constructor() {
     this.x = random(width);
     this.y = -50;
-    this.baseSpeed = random(2, 5);
+    this.speed = random(2, 5);
     this.angle = random(-PI / 6, PI / 6);
     this.color = color(random(100, 256), random(100, 256), random(100, 256));
-    this.vx = 0; // Velocity X
-    this.vy = 0; // Velocity Y
+    
   }
-  
+
   update() {
-    // Apply tilt influence to velocity
-    this.vx += tiltX * TILT_INFLUENCE;
-    this.vy = this.baseSpeed + (tiltY * TILT_INFLUENCE);
-    
-    // Add some drag to smooth movement
-    this.vx *= 0.95;
-    
-    // Update position
-    this.x += this.vx;
-    this.y += this.vy;
-    
-    // Bounce off walls
-    if (this.x < 0 || this.x > width) {
-      this.vx *= -0.5;
-      this.x = constrain(this.x, 0, width);
-    }
-  }
-  
+   
+        this.y += this.speed;
+        
+	}
+
   show() {
     push();
     translate(this.x, this.y);
-    rotate(this.angle + (tiltX * 0.5)); // Add tilt influence to rotation
+    rotate(this.angle);
     fill(255);
     rectMode(CENTER);
     rect(0, 0, 200, 60, 10);
@@ -92,8 +51,8 @@ class Notification {
     text("Look Here!", 0, 0);
     pop();
   }
-  
+
   offScreen() {
-    return this.y > height + 100;
+    return this.y > height;
   }
 }
